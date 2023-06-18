4acebec2e4da602b9bba15946277fb6c4a9b0b06
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-18 15:47:44 +0200

    page html

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 import { menuArray } from "./data.js";
 
 console.log(menuArray);
+
+function getDivHtml() {
+  let commandHtml = "";
+  menuArray.forEach((meal) => {
+    console.log(meal.ingredients);
+    meal.ingredients.forEach((furniture) => {
+      console.log(furniture);
+    });
+    commandHtml += `
+        <div class="command">
+          <div id="emoji">
+            <p class="emoji-size">${meal.emoji}</p>
+          </div>
+          <div id="details">
+            <h3>${meal.name}</h3>
+            <p class="ingredients">${meal.ingredients}</p>
+            <p class="price">$${meal.price}</p>
+          </div>
+          <div id="plus-btn">
+          <button>
+          <i class="data-plus fa-solid fa-plus"></i>
+        </button>
+          </div>
+        </div>
+      `;
+  });
+  return commandHtml;
+}
+
+function render() {
+  document.getElementById("section").innerHTML = getDivHtml();
+}
+render();
