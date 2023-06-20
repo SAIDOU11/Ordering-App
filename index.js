c8902e3643b7bd1cfc5091eb972a6175be8a005b
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-20 06:34:43 +0200

    refactoring

--- index.js
+++ index.js
@@ -4,25 +4,24 @@ let sectionOrder = document.querySelector(".section-order");
 let orderMeal = document.getElementById("order-meal");
 
 document.addEventListener("click", (e) => {
-  console.log(e.target.dataset.id);
-  //  &&e.target.dataset.id === "0"
-  if (e.target.dataset.id == "0" && e.target.id === "command-btn") {
-    console.log("BOUTON ", orderMeal);
+  if (e.target.dataset.add) {
+    console.log("Numéro ID du repas - ", e.target.dataset.add);
+    // console.log("BOUTON ", orderMeal);
     orderMeal.classList.remove("hidden");
     sectionOrder.classList.remove("hidden");
-    return getOrder(e.target.dataset.id);
-  } else if (e.target.id === "order-btn") {
-    modal.showModal();
-  } else if (e.target.id === "pay-btn") {
-    console.log("PAY");
-    const input = document.getElementsByTagName("input");
-    if (!input.value) {
-      console.log(input);
-      modal.close();
-      const thanks = document.querySelector(".thanks");
-      addToOrder.classList.add("hidden");
-      thanks.classList.remove("hidden");
-    }
+    return getOrder(e.target.dataset.add);
+    // } else if (e.target.id === "order-btn") {
+    //   modal.showModal();
+    // } else if (e.target.id === "pay-btn") {
+    //   console.log("PAY");
+    //   const input = document.getElementsByTagName("input");
+    //   if (!input.value) {
+    //     console.log(input);
+    //     modal.close();
+    //     const thanks = document.querySelector(".thanks");
+    //     addToOrder.classList.add("hidden");
+    //     thanks.classList.remove("hidden");
+    //   }
   }
 });
 
@@ -35,12 +34,12 @@ function getDivHtml() {
             <p class="emoji-size">${meal.emoji}</p>
           </div>
           <div id="details">
-            <h3 data-name>${meal.name}</h3>
-            <p class="ingredients">${meal.ingredients}</p>
-            <p data-price class="price">$${meal.price}</p>
+            <h3 id="meal">${meal.name}</h3>
+            <p  class="ingredients">${meal.ingredients}</p>
+            <p class="price">$${meal.price}</p>
           </div>
-          <div  id="plus-btn">
-          <button data-id="${meal.id}" id="command-btn">
+          <div id="plus-btn">
+          <button data-add="${meal.id}"   id="command-btn">
           +
         </button>
           </div>
@@ -62,22 +61,30 @@ function getOrder() {
     orderHtml += `
     <div id="order-content" class=" command-order">
           <div class="name-hidden">
-            <h3>Name <span class="remove">remove</span></h3>
+            <h3> ${meal.name} <span class="remove">remove</span></h3>
           </div>
           <div id="price">
-            <p class="price-order">$</p>
+            <p class="price-order">$${meal.price}</p>
           </div>
-
-          <div class=" total-price">
-            <div class="total">
-              <p>Total price:</p>
-            </div>
-            <div class="total">
-              <p>$</p>
-            </div>
           </div>
-        </div>
     `;
   }
   return (document.getElementById("order-meal").innerHTML = orderHtml);
 }
+
+// function renderTotal() {
+//   let totalHtml = "";
+//   totalHtml = `
+//   <div class=" total-price">
+//   <div class="total">
+//     <p>Total price:</p>
+//   </div>
+//   <div class="total">
+//     <p>$</p>
+//   </div>
+// </div>
+
+// `;
+//   totalHtml;
+// }
+// renderTotal();
